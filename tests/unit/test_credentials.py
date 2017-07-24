a1b0e3a6f3ead2c7a617d8e143bf4ef4ed4e1400
Ross Vandegrift <ross.vandegrift@cleardata.com>
2017-07-24 11:36:13 -0400

    fake_get_config_variable should handle the default methods case
    Without this change, fake_get_config_variable returns None for any call
    where methods is the default.  Since that includes all calls without an
    explicit profile, the new tests weren't testing the intended config.
    
    The previous tests in this class all have an explict profile set, and so
    end up calling fake_get_config_variable with a non-default methods.

--- tests/unit/test_credentials.py
+++ tests/unit/test_credentials.py
@@ -640,6 +640,9 @@ class TestCreateCredentialResolver(BaseEnvVar):
             return self.session_instance_vars.get(name)
         elif methods is not None and 'env' in methods:
             return self.fake_env_vars.get(name)
+        else:
+            return self.fake_env_vars.get(name) or \
+                self.session_instance_vars.get(name)
 
     def test_create_credential_resolver(self):
         resolver = credentials.create_credential_resolver(self.session)
